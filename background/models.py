b26ceadc2ca4e3bab75e1b98e47bcec426b8ed9c
BFlameSwift <19373732@buaa.edu.cn>
2021-05-26 09:02:23 +0800

    基本完成，待测试

--- background/models.py
+++ background/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.db import models
 from login.models import User
 
@@ -51,26 +53,42 @@ class Flight(models.Model):
     #
     #     ]
 
+
+
+    # 座位号与flight_id 决定 对应一个航班，（如果使用外键上下两个类互相外键无法引用
+    #
+
+
 class Concrete_flight(models.Model):
     # 由于航班的可重复性，把航班的时间设置成time格式， 但是需要每天的一个独立航班来有单次航班的预订次数
-    user_id = models.AutoField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     flight = models.ForeignKey(Flight,verbose_name="航班",on_delete=models.CASCADE)
     book_sum = models.IntegerField(verbose_name="订票总数", default=0)
     plane_capacity = models.IntegerField(verbose_name="飞机容量", blank=True) # 冗余列，方便起见
     flight_datetime = models.DateTimeField(verbose_name="航班时间")
-
+    # seat_chart = models.ForeignKey(FlightSeatingChart,on_delete=models.CASCADE,verbose_name="座位表",)
 
     # 利用
+class FlightSeatingChart(models.Model):
+    # 座位表，每一天的每一个航班都有一个，否则数组比较难实现，
+    # 以此来安排座位.(根据订票数来确定座位存在 bug
+    id = models.AutoField(primary_key=True)
+    seat_number = models.IntegerField(unique=False, verbose_name="座位号")
+    concrete_flight = models.ForeignKey(Concrete_flight,verbose_name="具体航班",on_delete=models.CASCADE)
+    # user = models.ForeignKey(User,verbose_name="用户",on_delete=models.CASCADE)
+    is_occupied = models.BooleanField(default=False,verbose_name="该座位是否被占用")
+
 
 class Order(models.Model):
     order_number = models.AutoField(primary_key=True,verbose_name = "订单编号")
     flight = models.ForeignKey(Flight,verbose_name="航班",on_delete=models.CASCADE)
     # flight_date = models.DateField(verbose_name="航班飞行日期")
-    flight_datetime = models.DateTimeField(verbose_name="航班飞行具体时间")
+    flight_datetime = models.DateTimeField(verbose_name="航班飞行具体时间",default=datetime.datetime.now())
     user = models.ForeignKey(User,verbose_name="用户",on_delete=models.CASCADE)
     price = models.FloatField(verbose_name="机票价格")
     order_time = models.DateTimeField(auto_now_add=True,verbose_name="订单创建时间")
     seat_number = models.IntegerField(verbose_name="座位号",)
+
     order_is_valid = models.BooleanField(verbose_name="订单是否有效",default=True)
     luggage_weight = models.FloatField(verbose_name="行李重量",default=0)
     #购买成功时生成订单，退订机票时为失效
@@ -90,7 +108,6 @@ class Order(models.Model):
 
     # class Meta:
         # da_table = 'order'
-
 
 
 
--- kernel/views.py
+++ kernel/views.py
@@ -1,5 +1,6 @@
 import datetime
 
+from django.db.models import Min,Count
 from django.shortcuts import render, redirect
 from django.forms.models import model_to_dict
 from django.views.decorators.csrf import csrf_exempt
@@ -11,7 +12,7 @@ import time
 
 import background.models
 from . import forms
-from background.models import Flight,Order,Concrete_flight
+from background.models import Flight,Order,Concrete_flight,FlightSeatingChart
 from login.models import User
 from .pays import get_pay
 
@@ -70,16 +71,23 @@ def book_ticket(request):
         user = User.objects.get(name=username)
         user_dict = model_to_dict(user)
 
+        if not setting_credit(user): # 函数在下面
+            message = '您的信用等级较低，无法订票'
+            return render(request, 'book_ticket.html', locals())
+
         money = float(request.POST.get('money'))
         flight_number = request.POST.get('flight_number')
-        date_str = str(request.POST.get('datetime'))
+        date_str = str(request.POST.get('date'))
         year,mouth,day = map(int,date_str.split('-'))
         date = datetime.date(year,mouth,day)
 
-        print(money,flight_number)
+        print(money,flight_number,date)
         try:
             flight = Flight.objects.get(flight_number = flight_number)
-            concrete_flight = Concrete_flight.objects.get(flight=flight,flight_datetime=datetime_str)
+            the_datetime = datetime.datetime()
+            the_datetime.date = date
+            the_datetime.time = flight.flight_time
+            concrete_flight = Concrete_flight.objects.get(flight=flight,flight_datetime=the_datetime)
             if user.balance < money:
                 # message = '余额不足，请充值，或请您选择是否支付宝付全款'
                 message = '余额不足，请使用支付宝充值'
@@ -87,7 +95,7 @@ def book_ticket(request):
                 return render(request, 'book_ticket.html', locals()) # 此时应还有个充值按钮
 
             # 所以这里使用函数来支付，因为当上面选择支付宝支付的时候也是成功支付了
-            pay_ticket(user,flight,date,money)
+            pay_ticket(user, flight, date, money)
             message = '支付成功'
             print(message)
             return render(request, 'book_ticket.html', locals())
@@ -115,14 +123,24 @@ def cancel_ticket(request):
             concrete_flight = Concrete_flight.objects.get(flight=flight,)
 
             # return render(request, 'cancel_ticket.html', locals())
-            order = Order.objects.get(flight=flight)
+            order = Order.objects.get(flight=flight,flight_datetime=concrete_flight.flight_datetime,user=user)
 
             order.order_is_valid = False
-            flight.book_sum -= 1
+            concrete_flight.book_sum -= 1
+            # flight.book_sum -= 1
             user.balance += order.price
             user.total_consumption -= order.price
+            #座位设置成False
+            seat = FlightSeatingChart.objects.get(concrete_flight=concrete_flight,seat_number=order.seat_number)
+            seat.is_occupied = False
+
             # TODO 不知道是不是欠考虑了什么
-            
+
+            user.save()
+            concrete_flight.save()
+            order.save()
+            seat.save()
+
             message = '退票成功'
             return render(request, 'cancel_ticket.html', locals())
 
@@ -160,18 +178,34 @@ def pay_ticket(user,flight,date,money,luggage_weight = 0):
     # flight.book_sum += 1 # 修改
     order = background.models.Order()
     # order.seat_number = flight.book_sum
-    order.seat_number = contrete_flight.book_sum
-    # 有问题，如果有人中途退票了就不能这么放了可能会出现相同的座位号
+    # 选座
+    new_seat = FlightSeatingChart()
+    try:
+        min_spare_seat = FlightSeatingChart.objects.filter(is_occupied=False,concrete_flight=contrete_flight).aggregate(Min('seat_number'))
+
+    except:
+        pass
+        max_occupied_seat = FlightSeatingChart.objects.filter(is_occupied=True,concrete_flight=contrete_flight).aggregate(Min('seat_number'))
+        min_spare_seat = max_occupied_seat + 1
+    order.seat_number = min_spare_seat
+    # 保存新座位
+    new_seat.seat_number = min_spare_seat
+    new_seat.is_occupied = True
+    new_seat.concrete_flight = contrete_flight
+    new_seat.save()
+    # order.seat_number = contrete_flight.book_sum
+    # 有问题，如果有人中途退票了就不能这么放了可能会出现相同的座位号, 于是改成了表的形式
     order.user = user 
     order.flight = flight
     order.order_time = datetime.datetime.now()
+    order.flight_datetime = contrete_time
     order.flight_type = get_flight_type(money,flight)
     order.price = money
     order.order_is_valid = True
     order.luggage_weight = luggage_weight
     # TODO 根据行李重量增加钱，应该至少在get_flight_type 后判断，否则加了钱就不能按照钱来找对应的航班了
     if luggage_weight >20:
-        money += 18 *(luggage_weight-20)
+        money += 18 *(luggage_weight-20)# 亲测大兴机场价格
 
 
 
@@ -222,3 +256,43 @@ def paysView(request):
         return render(request, 'book_ticket.html', locals())
         # return JsonResponse({'message': '你的请求不是POST'})
         # return redirect('shopper:shopcart')# TODO ###
+
+def credit_evaluation(user):
+    orders = Order.objects.filter(user=user)
+    count_broken_promises = int(0)
+    for order in orders:
+        order_create_time = order.order_time
+        if (datetime.datetime.now()-order_create_time).days <= 30:
+            count_broken_promises += 1
+    if count_broken_promises >= 3:
+        return int(1) # 信用C
+    elif count_broken_promises<3 and count_broken_promises > 0:
+        return int(2) # 信用B
+    else:
+        if user.total_consumption > 100000:
+            return 5 # AAA
+        elif user.total_consumption>10000 and user.total_consumption <=100000:
+            return 4 # AA
+        else:
+            return 3 # A
+
+def setting_credit(user):
+    # True 可以订票，False 不可订票
+    ret = int(credit_evaluation(user))
+    # user.credit_rating = (int(ret) + int('0')) # 对应信用
+    if ret == 5:
+        user.credit_rating = '5'
+    elif ret == 4:
+        user.credit_rating = '4'
+    elif ret == 3:
+        user.credit_rating = '3'
+    elif ret == 2:
+        user.credit_rating = '2'
+    elif ret == 1:
+        user.credit_rating = '1'
+    else:
+        user.credit_rating = '3'
+    if ret > 1:
+        return True
+    else:
+        return False
